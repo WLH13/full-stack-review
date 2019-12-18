fcf81e3e7eb18495f5a47f70497f50ccd89223ad
Matt Bodily <mattcbodily@gmail.com>
2019-12-18 16:54:04 -0700

    finished review code

---
+++ server/cartController.js
@@ -0,0 +1,23 @@
+module.exports = {
+    getProducts: (req, res) => {
+        const db = req.app.get('db');
+        db.get_products()
+        .then(products => {res.status(200).send(products)})
+        .catch(err => res.status(500).send(err))
+    },
+    addToCart: (req, res) => {
+        const {customer_order_id, product_id, price} = req.body;
+        const db = req.app.get('db');
+        db.orders.add_to_cart({order_id: customer_order_id, product_id, price})
+        .then(res => res.sendStatus(200))
+        .catch(err => res.status(500).send(err))
+    },
+    getCart: (req, res) => {
+        const {id} = req.params;
+        const db = req.app.get('db');
+        db.orders.get_cart(id).then(cart => {
+            res.status(200).send(cart)
+        })
+        .catch(err => res.status(500).send(err))
+    }
+}
